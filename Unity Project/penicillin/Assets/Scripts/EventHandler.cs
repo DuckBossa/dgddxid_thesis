30081a3e10ff900d2e7cd1bc979f6502fd7282a5
Raphael Angelo S. Reventar <DuckBossa@gmail.com>
2016-09-01 16:23:54 +0800

    Dummy Event Handler

---
+++ Unity Project/penicillin/Assets/Scripts/EventHandler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System;
+using System.IO;
+using System.Collections;
+
+public class EventHandler : MonoBehaviour {
+
+	public static event Action Attack;
+	public static event Action<Vector3> Move;
+	public static event Action Jump;
+	public static event Action UseItem;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
