9dff027c2d48979c0848b71c6ade4ea43aa4bfc8
Raphael <DuckBossa@gmail.com>
2016-09-05 18:02:29 +0800

    preliminary jumping and movement

--- Unity Project/penicillin/Assets/Scripts/GAME.cs
+++ Unity Project/penicillin/Assets/Scripts/GAME.cs
@@ -5,7 +5,9 @@ namespace GLOBAL{
     
     public class GAME : MonoBehaviour {
         public const int tile_size = 64;
-        public const float player_velocity = 5f;
+        public const float player_velocity = 4f;
+        public const int jumps = 2;
+        public const float jump_force = 250f;
     }
 
 }
--- Unity Project/penicillin/Assets/Scripts/PlayerMovement.cs
+++ Unity Project/penicillin/Assets/Scripts/PlayerMovement.cs
@@ -1,26 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using GLOBAL;
 public class PlayerMovement : MonoBehaviour {
   
     float dir;
     Animator anim;
     Rigidbody2D rb;
-
+    int currjumps;
     void Start(){
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
-
+        currjumps = GAME.jumps;
         dir = 0;
     }
 
     void Update(){
         dir = Input.GetAxis("Horizontal");
+        bool jumping = Input.GetKeyDown(KeyCode.W);
         anim.SetFloat("Direction", dir);
-
-        
+        Vector3 movement = new Vector3(dir * GAME.player_velocity * Time.deltaTime, 0, 0);
+        transform.Translate(movement);
+        if(jumping && currjumps > 0){
+            Jump();
+        }
 
     }
+    void OnCollisionEnter2D(Collision2D coll){
+        
+        bool onTop = coll.transform.position.y > transform.position.y;
+        if (!onTop){
+            Debug.Log("RESET");
+            currjumps = GAME.jumps;
+        }
+            
+    }
 
+    void Jump() {
+        rb.AddForce(Vector2.up * GAME.jump_force);
+        currjumps--;
+    }
 
 }
