c67655fa39d1461d2e2f6d54301efb3652d95e46
Raphael <DuckBossa@gmail.com>
2016-09-05 14:14:23 +0800

    Fixed Git Ignore

---
+++ penicillin/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+    public const float VELOCITY = 5f;
+    float dir;
+    Animator anim;
+    
+
+    void Start(){
+        anim = GetComponent<Animator>();
+        dir = 0;
+    }
+
+    void Update(){
+        dir = Input.GetAxis("Horizontal");
+        anim.SetFloat("Direction", dir);
+    }
+
+
+}
