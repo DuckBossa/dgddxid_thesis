5c46f091d24deb6e4d8e4eee1a27834c63ede04d
Melvin Luis Mendoza <melvinluismendoza@gmail.com>
2016-10-03 00:37:11 +0800

    Initial shigellah animation + platform detection

---
+++ Unity Project/penicillin/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour {
+    public LayerMask enemyMask;
+    public float speed = 1;
+    Rigidbody2D myBody;
+    Transform myTrans;
+    float myWidth, myHeight;
+
+    void Start() {
+        myTrans = this.transform;
+        myBody = this.GetComponent<Rigidbody2D>();
+        SpriteRenderer mySprite = this.GetComponent<SpriteRenderer>();
+        myWidth = mySprite.bounds.extents.x;
+        myHeight = mySprite.bounds.extents.y;
+    }
+
+    void FixedUpdate() {
+        Vector2 lineCastPos = toVector2(myTrans.position) - toVector2(myTrans.right) * myWidth + Vector2.up * myHeight;
+        bool isGrounded = Physics2D.Linecast(lineCastPos, lineCastPos + Vector2.down, enemyMask);
+        bool isBlocked = Physics2D.Linecast(lineCastPos, lineCastPos - toVector2(myTrans.right) * .05f, enemyMask);
+        if (!isGrounded || isBlocked) {
+            Vector3 currRot = myTrans.eulerAngles;
+            currRot.y += 180;
+            myTrans.eulerAngles = currRot;
+        }
+        Vector2 myVel = myBody.velocity;
+        myVel.x = -myTrans.right.x * speed;
+        myBody.velocity = myVel;
+    }
+
+    Vector2 toVector2(Vector3 vec3) {
+        return new Vector2(vec3.x, vec3.y);
+    }
+}
