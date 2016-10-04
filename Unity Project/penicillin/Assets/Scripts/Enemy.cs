8b545f2bcd08032b35fdc8e28d77444fdc194799
Melvin Luis Mendoza <melvinluismendoza@gmail.com>
2016-10-04 20:45:11 +0800

    Walking shigella AI (patrol + attack animation)

--- Unity Project/penicillin/Assets/Scripts/Enemy.cs
+++ Unity Project/penicillin/Assets/Scripts/Enemy.cs
@@ -8,7 +8,8 @@ public class Enemy : MonoBehaviour {
     Transform myTrans, penny;
     float myWidth, myHeight;
     public float aggressionDistance;
-    public bool isAggro = false;
+    public bool isAggro;
+    bool withinRange;
     public float atkRange;
     SpriteRenderer psprite;
     Animator myAnim;
@@ -25,21 +26,23 @@ public class Enemy : MonoBehaviour {
         penny = p.transform;
         psprite = p.GetComponent<SpriteRenderer>();
         aggressionDistance = 2f;
-        atkRange = .15f;
+        atkRange = .25f;
         speed = .5f;
+        isAggro = false;
+        withinRange = false;
         myAnim = GetComponent<Animator>();
     }
 
     void FixedUpdate() {
+        Vector2 lineCastPos = toVector2(myTrans.position) - toVector2(myTrans.right) * myWidth + Vector2.up * myHeight;
+        bool isGrounded = Physics2D.Linecast(lineCastPos, lineCastPos + Vector2.down, enemyMask);
+        bool isBlocked = Physics2D.Linecast(lineCastPos, lineCastPos - toVector2(myTrans.right) * .05f, enemyMask);
+        if (!isGrounded || isBlocked) {
+            Vector3 currRot = myTrans.eulerAngles;
+            currRot.y += 180;
+            myTrans.eulerAngles = currRot;
+        }
         if (!isAggro) {
-            Vector2 lineCastPos = toVector2(myTrans.position) - toVector2(myTrans.right) * myWidth + Vector2.up * myHeight;
-            bool isGrounded = Physics2D.Linecast(lineCastPos, lineCastPos + Vector2.down, enemyMask);
-            bool isBlocked = Physics2D.Linecast(lineCastPos, lineCastPos - toVector2(myTrans.right) * .05f, enemyMask);
-            if (!isGrounded || isBlocked) {
-                Vector3 currRot = myTrans.eulerAngles;
-                currRot.y += 180;
-                myTrans.eulerAngles = currRot;
-            }
             Vector2 myVel = myBody.velocity;
             myVel.x = -myTrans.right.x * speed;
             myBody.velocity = myVel;
@@ -50,23 +53,68 @@ public class Enemy : MonoBehaviour {
             }
         }
         else {
-            atkRange = Mathf.Abs(penny.position.x - myTrans.position.x);
-            if (atkRange > aggressionDistance || Mathf.Abs(myTrans.position.y - penny.position.y) > 0.4f) {
-                isAggro = false;
-                myAnim.SetBool("isAggro", isAggro);
+            float dist = Mathf.Abs(penny.position.x - myTrans.position.x); //distance between penny and enemy
+            withinRange = dist < atkRange ? true : false; //am I gonna play the attack animation or not?
+            myAnim.SetBool("withinRange", withinRange); //let the animator know that it's attack time
+            if (dist > aggressionDistance || Mathf.Abs(myTrans.position.y - penny.position.y) > 0.4f) {
+                isAggro = false; 
+                myAnim.SetBool("isAggro", isAggro); //to change animation, go back to idle/roam
+            }
 
-            }
-            //penny left
-            if (penny.position.x - myTrans.position.x > 0) {
-                Vector2 myVel = myBody.velocity;
-                myVel.x = myTrans.right.x * speed * 2;
-                myBody.velocity = myVel;
-            }
             //penny right
+            bool facingRight = false;
+            if (penny.position.x - myTrans.position.x > 0) {
+                if (!withinRange) {
+                    Vector2 myVel = myBody.velocity;
+                    myVel.x = myTrans.right.x * speed * 2;
+                    //if the bacteria is moving towards penny
+                    if (myVel.x < 0) {
+                        myVel.x *= -1;
+                    }
+                    else {
+                        Vector3 currRot = myTrans.eulerAngles;
+                        currRot.y += 180;
+                        myTrans.eulerAngles = currRot;
+                    }
+                    myBody.velocity = myVel;
+                }
+                else {
+                    Vector2 myVel = myBody.velocity;
+                    myVel.x = 0;
+                    myBody.velocity = myVel;
+
+                    Vector3 currRot = myTrans.eulerAngles;
+                    currRot.y += 180;
+                    myTrans.eulerAngles = currRot;
+                }
+            }
+
+
+            //penny left
             else {
-                Vector2 myVel = myBody.velocity;
-                myVel.x = -myTrans.right.x * speed * 2;
-                myBody.velocity = myVel;
+                if (!withinRange) {
+                    Vector2 myVel = myBody.velocity;
+                    myVel.x = myTrans.right.x * speed * 2;
+                    //if the bacteria is moving towards penny
+                    if (myVel.x > 0) {
+                        myVel.x *= -1;
+                    }
+                    else {
+                        Vector3 currRot = myTrans.eulerAngles;
+                        currRot.y += 180;
+                        myTrans.eulerAngles = currRot;
+                    }
+                    myBody.velocity = myVel;
+                }
+                else {
+                    Vector2 myVel = myBody.velocity;
+                    myVel.x = 0;
+                    myBody.velocity = myVel;
+
+                    Vector3 currRot = myTrans.eulerAngles;
+                    currRot.y += 180;
+                    myTrans.eulerAngles = currRot;
+                }
             }
         }
     }
