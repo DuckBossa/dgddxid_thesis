845e773a8e95e4325e5d3a0f7739c8d7a33fd61e
Raphael <DuckBossa@gmail.com>
2016-10-05 12:28:49 +0800

    tile construction.cs

--- Unity Project/penicillin/Assets/Scripts/TileDeconstructionConstruction.cs
+++ Unity Project/penicillin/Assets/Scripts/TileDeconstructionConstruction.cs
@@ -4,14 +4,17 @@ using System.Collections.Generic;
 
 public class TileDeconstructionConstruction : MonoBehaviour {
 
+    public int numTileToGenerate = 5;
     public List<GameObject> tiles = new List<GameObject>();
+    public List<GameObject> spawn_points = new List<GameObject>();
+    public GameObject parent_dynamic_tile;
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    public void reconstruct() {
+
+            
+    }
 }
