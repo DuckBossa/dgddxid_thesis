b55aadd27c4d642b93cb58dbe2cedf04629946dc
Melvin Luis Mendoza <melvinluismendoza@gmail.com>
2016-10-10 00:03:26 +0800

    Quit and restart (restart needs fixing)

--- Unity Project/penicillin/Assets/Scripts/PauseHandler.cs
+++ Unity Project/penicillin/Assets/Scripts/PauseHandler.cs
@@ -1,15 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PauseHandler : MonoBehaviour {
 
     public Transform pauseCanvas;
     public Transform audioToggle;
+    public Transform quitPrompt;
+    public Transform pauseMenu;
+    public Transform restartPrompt;
     bool soundOn = true;
 	
 	void Start () {
         pauseCanvas.gameObject.SetActive(false);
+        quitPrompt.gameObject.SetActive(false);
+        restartPrompt.gameObject.SetActive(false);
 	}
 	
 	void Update () {
@@ -17,9 +23,7 @@ public class PauseHandler : MonoBehaviour {
 	}
 
     public void PauseButton() {
-        //if Pause menu is not active, activate; else, deactivate
         pauseCanvas.gameObject.SetActive(pauseCanvas.gameObject.activeInHierarchy ? false : true);
-        //if Pause menu is active, set time to 0; else, set to 1
         Time.timeScale = pauseCanvas.gameObject.activeInHierarchy ? 0 : 1;
     }
 
@@ -28,7 +32,21 @@ public class PauseHandler : MonoBehaviour {
         audioToggle.GetComponent<Text>().text = soundOn ? "Sound: On" : "Sound: Off";
     }
 
-    public void RestartLevel() {
+    public void QuitPrompt() {
+        quitPrompt.gameObject.SetActive(quitPrompt.gameObject.activeInHierarchy ? false : true);
+        pauseMenu.gameObject.SetActive(pauseMenu.gameObject.activeInHierarchy ? false : true);
+    }
 
+    public void RestartPrompt() {
+        restartPrompt.gameObject.SetActive(restartPrompt.gameObject.activeInHierarchy ? false : true);
+        pauseMenu.gameObject.SetActive(pauseMenu.gameObject.activeInHierarchy ? false : true);
+    }
+
+    public void RestartLevel() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ExitApplication() {
+        Application.Quit();
     }
 }
