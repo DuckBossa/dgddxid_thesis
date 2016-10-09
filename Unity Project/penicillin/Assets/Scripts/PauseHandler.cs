c08b4a0b7657804a0702f16fab551fa01d665f57
Melvin Luis Mendoza <melvinluismendoza@gmail.com>
2016-10-09 19:50:15 +0800

    Pause menu (no functionality yet)

---
+++ Unity Project/penicillin/Assets/Scripts/PauseHandler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseHandler : MonoBehaviour {
+
+    public Transform pauseCanvas;
+	
+	void Start () {
+        pauseCanvas.gameObject.SetActive(false);
+	}
+	
+	void Update () {
+	    
+	}
+
+    public void PauseButton() {
+        //if Pause menu is not active, activate; else, deactivate
+        pauseCanvas.gameObject.SetActive(pauseCanvas.gameObject.activeInHierarchy ? false : true);
+    }
+}
