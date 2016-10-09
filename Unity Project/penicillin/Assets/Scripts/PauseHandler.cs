0a9077557b0be53b588f9321b15855a020441343
Melvin Luis Mendoza <melvinluismendoza@gmail.com>
2016-10-09 20:37:55 +0800

    Pause menu has some functionality
    Resume works
    Sound toggle toggles, but with no sound LMAO
    
    To do:
    Restart level has no pop-up window yet
    Exit to Main Menu: no main menu lol
    Exit Game: doesn't kill game yet (no popup for confirmation)

--- Unity Project/penicillin/Assets/Scripts/PauseHandler.cs
+++ Unity Project/penicillin/Assets/Scripts/PauseHandler.cs
@@ -1,9 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PauseHandler : MonoBehaviour {
 
     public Transform pauseCanvas;
+    public Transform audioToggle;
+    bool soundOn = true;
 	
 	void Start () {
         pauseCanvas.gameObject.SetActive(false);
@@ -16,5 +19,16 @@ public class PauseHandler : MonoBehaviour {
     public void PauseButton() {
         //if Pause menu is not active, activate; else, deactivate
         pauseCanvas.gameObject.SetActive(pauseCanvas.gameObject.activeInHierarchy ? false : true);
+        //if Pause menu is active, set time to 0; else, set to 1
+        Time.timeScale = pauseCanvas.gameObject.activeInHierarchy ? 0 : 1;
+    }
+
+    public void ToggleAudio() {
+        soundOn = !soundOn;
+        audioToggle.GetComponent<Text>().text = soundOn ? "Sound: On" : "Sound: Off";
+    }
+
+    public void RestartLevel() {
+
     }
 }
