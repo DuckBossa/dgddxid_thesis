eb31f9fdaebf3b0013f2a55c0eb64f5fb9d13415
Raphael Angelo S. Reventar <DuckBossa@gmail.com>
2016-10-29 11:41:06 +0800

    tiledeconstruction reworked

---
+++ Unity Project/penicillin/Assets/Scripts/TileDeconstruction.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TileDeconstruction : MonoBehaviour {
+
+	BoxCollider2D bounderinos;
+	List<Collider2D>tiles;
+	void Start(){
+		bounderinos = GetComponent<BoxCollider2D> ();
+		tiles = new List<Collider2D> ();
+	}
+
+	void FixedUpdate(){
+		for (int i = tiles.Count - 1; i >= 0; i--) {
+			if (bounderinos.bounds.max.y > tiles [i].bounds.max.y) {
+				tiles [i].gameObject.SetActive (false);
+				tiles.RemoveAt (i);
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag.Equals ("Tiles")) {
+			tiles.Add (other);
+		}
+	}	
+}
