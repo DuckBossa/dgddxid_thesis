3d3a5d504b545cfff350910826247a90307239c8
Melvin Luis Mendoza <melvinluismendoza@gmail.com>
2016-10-30 19:56:08 +0800

    Minimap camera size and position adjust according to screen resolution, viewport independent of screen resolution (always views the whole map)

---
+++ Unity Project/penicillin/Assets/MinimapScaler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinimapScaler : MonoBehaviour {
+
+    public Camera cam;
+    float width, height;
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        width = Screen.width / 3.9f;
+        height = (width / 24.15f) * 9f;
+        float ratio = (float)Screen.width / (float)Screen.height;
+        cam.pixelRect = new Rect(Screen.width - (width + 15), Screen.height - (height + 45f * ratio), width, height);
+        cam.orthographicSize = 5.38f;
+    }
+}
