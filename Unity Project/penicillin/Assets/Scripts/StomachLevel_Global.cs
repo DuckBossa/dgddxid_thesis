a2dc87e5f0f0d21e22b09a7c9df418d47e637e65
Raphael <DuckBossa@gmail.com>
2016-11-11 17:48:54 +0800

    On-Screen timer

--- Unity Project/penicillin/Assets/Scripts/StomachLevel_Global.cs
+++ Unity Project/penicillin/Assets/Scripts/StomachLevel_Global.cs
@@ -7,12 +7,22 @@ public class StomachLevel_Global : MonoBehaviour {
     // Use this for initialization
     public Text screenTimer;
     public float timeLimitInMinutes;
+    private float timeLimitInSeconds;
 	void Start () {
         Time.timeScale = 1;
+        timeLimitInSeconds = 60*timeLimitInMinutes;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         globalTime += Time.deltaTime; //time in seconds
+        float timeRemaining = timeLimitInSeconds - globalTime;
+        string min = Mathf.Floor(timeRemaining / 60).ToString("00");
+        string sec = (timeRemaining % 60).ToString("00");
+        screenTimer.text = min + ":" + sec;
+
+        if(timeLimitInSeconds == 0) {
+            //trigger an event
+        }
 	}
 }
