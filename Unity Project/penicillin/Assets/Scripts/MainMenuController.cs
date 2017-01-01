dad89f97baab18b4e87383ede8010a007591b137
Melvin Luis Mendoza <melvinluismendoza@gmail.com>
2017-01-01 19:54:53 +0800

    New UI Elements

--- Unity Project/penicillin/Assets/Scripts/MainMenuController.cs
+++ Unity Project/penicillin/Assets/Scripts/MainMenuController.cs
@@ -5,15 +5,14 @@ using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour {
 
-    public Transform quitPrompt;
-    public Transform mainMenu;
+    public Transform t_quit, t_menu, t_tutorial, t_credits;
+    public Button b_start, b_quit, b_tutorial, b_credits;
     public AudioClip buttonPress;
-    public Button start, tutorial, quitgame;
     AudioSource audio;
     bool started;
 
     void Start() {
-        quitPrompt.gameObject.SetActive(false);
+        t_quit.gameObject.SetActive(false);
         audio = GameObject.Find("ButtonAudio").GetComponent<AudioSource>();
     }
 
@@ -28,15 +27,25 @@ public class MainMenuController : MonoBehaviour {
 
     public void QuitPrompt() {
         audio.Play();
-        quitPrompt.gameObject.SetActive(quitPrompt.gameObject.activeInHierarchy ? false : true);
-        start.interactable = quitPrompt.gameObject.activeInHierarchy ? false : true;
-        tutorial.interactable = quitPrompt.gameObject.activeInHierarchy ? false : true;
-        quitgame.interactable = quitPrompt.gameObject.activeInHierarchy ? false : true;
+        toggleUI(t_quit);
     }
 
     public void Tutorial() {
         audio.Play();
-        SceneManager.LoadScene("Tutorial");
+        toggleUI(t_tutorial);
+    }
+
+    public void Credits() {
+        audio.Play();
+        toggleUI(t_credits);
+    }
+
+    void toggleUI(Transform obj) {
+        obj.gameObject.SetActive(obj.gameObject.activeInHierarchy ? false : true);
+        b_start.interactable = obj.gameObject.activeInHierarchy ? false : true;
+        b_tutorial.interactable = obj.gameObject.activeInHierarchy ? false : true;
+        b_quit.interactable = obj.gameObject.activeInHierarchy ? false : true;
+        b_tutorial.interactable = obj.gameObject.activeInHierarchy ? false : true;
     }
 
     public void ExitApplication() {
